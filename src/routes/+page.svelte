546a510e1d9d136d925d5a6fbdf862fb646777c8
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-09-08 13:57:55 +0200

    Set up Sveltekit V2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,9 @@
+
 <script>
-	export let data;
-</script>
-
-<main>
-    <article class="front cover">
-        <section>
-            <h1></h1>
-            <p></p>
-            <p></p>
-        </section>
-       
-    </article>
-
-    <article class="back cover">
-        <section>
-         <p></p>
-         <p></p>
-         <p></p>
-       <div><button></button></div>
-        </section>
-    </article>
-
-    <article class="left cover ">
-        <p></p>
-    </article>
-    <article class="right cover">
-        <p></p>
-    </article>
-    <article class="bottom cover">
-        <section>
-            <a href="/" alt="twitter">
-                <svg>
-                 
-                </svg>
-            </a>
-            <a href="/" alt="LinkedIn">
-                <svg>
-              
-                </svg>
-            </a>
-            <a href="/" alt="Github">
-                <svg>
-                  
-                </svg>
-            </a>
-        </section>
-    </article>
-    <article class="top cover"></article>
-</main>
+        import { SliceZone } from '@prismicio/svelte'
+        import { components } from '$lib/slices'
+        export let data
+        console.log(data)
+    </script>
+    
+    <SliceZone slices={data.slices} {components} />
