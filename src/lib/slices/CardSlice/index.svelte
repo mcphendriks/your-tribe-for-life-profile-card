f7aa9dabc82b2f46ab1833324fbb8eff5990899b
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-10-12 16:34:22 +0200

    Fix HTML Validator issues

--- src/lib/slices/CardSlice/index.svelte
+++ src/lib/slices/CardSlice/index.svelte
@@ -1,17 +1,16 @@
 <script>
-
   import { onMount } from 'svelte'
 
   onMount(() => {
-   let cardSlice = document.getElementById('card')
-   let  x = document.getElementById('x')
-   let  y = document.getElementById('y')
+    let cardSlice = document.getElementById('card')
+    let x = document.getElementById('x')
+    let y = document.getElementById('y')
 
     window.onmousemove = (e) => {
-      const degX = ((e.clientX / window.innerWidth) * 360) - 180
-      const degY = ((e.clientY / window.innerHeight) * 360) - 180
+      const degX = (e.clientX / window.innerWidth) * 360 - 180
+      const degY = (e.clientY / window.innerHeight) * 360 - 180
 
-    cardSlice.style.transform = `rotateY(${degX}deg) rotateX(${degY}deg)`
+      cardSlice.style.transform = `rotateY(${degX}deg) rotateX(${degY}deg)`
 
       const fx = Math.floor(degX)
       const fy = Math.floor(degY)
@@ -19,184 +18,177 @@
       x.innerText = `X: ${fx}`
       y.innerText = `Y: ${fy}`
     }
-
   })
 
-  export let slice;
-
+  export let slice
 </script>
 
-
-<article data-slice-type={slice.slice_type} data-slice-variation={slice.variation} id="card">
- 
+<article
+  data-slice-type={slice.slice_type}
+  data-slice-variation={slice.variation}
+  id="card"
+>
   <section class="front cover">
-         <h1>{slice.primary.cardtitle[0].text}</h1> 
-        <p>{slice.primary.cardparagraph[0].text}</p>
-        <p>{slice.primary.card_text[0].text}</p>
-        <p>{slice.primary.cardlogo[0].text}</p>
+    <h2>{slice.primary.cardtitle[0].text}</h2>
+    <p>{slice.primary.cardparagraph[0].text}</p>
+    <p>{slice.primary.card_text[0].text}</p>
+    <p>{slice.primary.cardlogo[0].text}</p>
   </section>
 
-<section class="back cover"></section>
-
-<section class="left cover"></section>
-
-<section class="right cover"></section>
-
-<section class="bottom cover"></section>
-
-<section class="top cover"></section>
-
+  <section class="back cover" />
+  <section class="left cover" />
+  <section class="right cover" />
+  <section class="bottom cover" />
+  <section class="top cover" />
 </article>
 
-<div class="blob"></div>
-
+<div class="blob" />
 
 <style>
-
   article {
-    width:25rem;
-    height:35rem;
-    background: #F1CFB2ff;
+    width: 25rem;
+    height: 35rem;
+    background: #f1cfb2ff;
     font-family: 'Inconsolata', monospace;
-    transform-style:preserve-3d;
-    transform:rotate3d(0,0,0,190deg);
-    position:relative;
-  }
-  
-  .cover {
-  position:absolute;
-  top:0;
-  left:0;
-  width:100%;
-  height:100%;
-  box-shadow: -5px -5px 9px  rgb(213, 179, 148), 5px 5px 9px  rgb(234, 174, 141)
+    transform-style: preserve-3d;
+    transform: rotate3d(0, 0, 0, 190deg);
+    position: relative;
   }
 
-  .front{
-    transform:translateZ(50px) translateX(0px) translateY(0px);
+  .cover {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    box-shadow: -5px -5px 9px rgb(213, 179, 148), 5px 5px 9px rgb(234, 174, 141);
   }
- 
+
+  .front {
+    transform: translateZ(50px) translateX(0px) translateY(0px);
+  }
+
   .front h1 {
-    font-size: 2.5em; 
+    font-size: 2.5em;
   }
 
   .front p {
-   font-family: 'Inconsolata', monospace;
-   letter-spacing: 1.4px;
+    font-family: 'Inconsolata', monospace;
+    letter-spacing: 1.4px;
   }
 
   .front p:last-child {
     text-align: right;
-   margin-right: 1.5rem;
+    margin-right: 1.5rem;
     font-weight: 600;
     font-size: 2.5em;
-    animation: letterspacing 10s infinite alternate cubic-bezier(.2, 0, 0, 1);
+    animation: letterspacing 10s infinite alternate cubic-bezier(0.2, 0, 0, 1);
   }
 
-  .back{
-    transform:translateZ(0px) translateX(0px) translateY(0px) rotateY(180deg);
+  .back {
+    transform: translateZ(0px) translateX(0px) translateY(0px) rotateY(180deg);
   }
 
-  .left{
-    transform:translateZ(25px) translateX(-25px) translateY(0px) rotateY(-90deg);
-  width:50px;
+  .left {
+    transform: translateZ(25px) translateX(-25px) translateY(0px)
+      rotateY(-90deg);
+    width: 50px;
   }
 
-  .right{
-  transform:translateZ(25px) translateX(-25px) translateY(0px) rotateY(90deg);
-  width:50px;
-  border-top:1px solid #F1CFB2ff;
-  border-bottom:1px solid rgb(233, 194, 160);
+  .right {
+    transform: translateZ(25px) translateX(-25px) translateY(0px) rotateY(90deg);
+    width: 50px;
+    border-top: 1px solid #f1cfb2ff;
+    border-bottom: 1px solid rgb(233, 194, 160);
   }
 
-  .top{
-  transform:translateZ(25px) translateX(0px) translateY(-25px) rotateX(90deg);
-  height:50px;
+  .top {
+    transform: translateZ(25px) translateX(0px) translateY(-25px) rotateX(90deg);
+    height: 50px;
   }
 
-  .bottom{
-  transform:translateZ(25px) translateX(0px) translateY(528px) rotateX(90deg);
-  height:50px;
+  .bottom {
+    transform: translateZ(25px) translateX(0px) translateY(528px) rotateX(90deg);
+    height: 50px;
   }
 
   .blob {
-  position: absolute;
-  width: 500px;
-  height: 500px;
-  background: linear-gradient(
-    180deg,
-    rgba(250, 225, 204, 0.42) 31.77%,
-    #edbf98 100%
-  );
-  mix-blend-mode: color-dodge;
-  animation: move 10s infinite alternate;
-  transition: 1s cubic-bezier(0.07, 0.8, 0.16, 1);
+    position: absolute;
+    width: 500px;
+    height: 500px;
+    background: linear-gradient(
+      180deg,
+      rgba(250, 225, 204, 0.42) 31.77%,
+      #edbf98 100%
+    );
+    mix-blend-mode: color-dodge;
+    animation: move 10s infinite alternate;
+    transition: 1s cubic-bezier(0.07, 0.8, 0.16, 1);
   }
 
   .blob:hover {
-  width: 520px;
-  height: 520px;
-  filter: blur(30px);
+    width: 520px;
+    height: 520px;
+    filter: blur(30px);
   }
 
-@keyframes letterspacing {
-  0% {
-    letter-spacing: -3rem;
-  }
-  
-  100% {
-    letter-spacing: 0.5rem;
-  }
-} 
+  @keyframes letterspacing {
+    0% {
+      letter-spacing: -3rem;
+    }
 
-@keyframes move {
-  from {
-    transform: translate(190px, -40px) rotate(-60deg);
-    border-radius: 24% 76% 35% 65% / 27% 36% 64% 73%;
+    100% {
+      letter-spacing: 0.5rem;
+    }
   }
 
-  to {
-    transform: translate(-190px, 100px) rotate(-10deg);
-    border-radius: 76% 24% 33% 67% / 68% 55% 45% 32%;
+  @keyframes move {
+    from {
+      transform: translate(190px, -40px) rotate(-60deg);
+      border-radius: 24% 76% 35% 65% / 27% 36% 64% 73%;
+    }
+
+    to {
+      transform: translate(-190px, 100px) rotate(-10deg);
+      border-radius: 76% 24% 33% 67% / 68% 55% 45% 32%;
+    }
   }
-}
 
-@media screen and (min-width: 0em) and (max-width: 45em){
-  
-article{
-  width:300px;
-  height:500px;
-}
+  @media screen and (min-width: 0em) and (max-width: 45em) {
+    article {
+      width: 300px;
+      height: 500px;
+    }
 
-.front h1 {
-  font-size: 1.5em;
-}
+    .front h2 {
+      font-size: 1.5em;
+    }
 
-.front p:first-child {
-  font-size: 0.1em;
-}
+    .front p:first-child {
+      font-size: 0.1em;
+    }
 
-.front p:last-child {
-  font-size: 1.5em
-}
+    .front p:last-child {
+      font-size: 1.5em;
+    }
 
-.front{
-    transform:translateZ(50px) translateX(0px) translateY(0px);
-}
+    .front {
+      transform: translateZ(50px) translateX(0px) translateY(0px);
+    }
 
-.left{
-    transform:translateZ(25px) translateX(-25px) translateY(0px) rotateY(-90deg);
-}
+    .left {
+      transform: translateZ(25px) translateX(-25px) translateY(0px)
+        rotateY(-90deg);
+    }
 
-.right{
-    transform:translateZ(25px) translateX(279px) translateY(0px) rotateY(90deg);
-}
-  
-.bottom{
-    transform:translateZ(25px) translateX(0px) translateY(476px) rotateX(90deg);
-}
+    .right {
+      transform: translateZ(25px) translateX(279px) translateY(0px)
+        rotateY(90deg);
+    }
 
-}
-
+    .bottom {
+      transform: translateZ(25px) translateX(0px) translateY(476px)
+        rotateX(90deg);
+    }
+  }
 </style>
-
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,26 @@
 <main>
-   <slot/>
+  <slot />
 </main>
 
 <style>
+  * {
+    background: rgb(206, 101, 56);
+    background: radial-gradient(
+      circle,
+      rgba(206, 101, 56, 1) 0%,
+      rgba(223, 154, 118, 1) 0%,
+      rgba(229, 172, 139, 1) 49%,
+      rgba(236, 191, 160, 1) 85%,
+      rgba(241, 207, 178, 1) 100%
+    );
+  }
 
   main {
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  min-height:100vh;
-  cursor:grabbing;
-  user-select:none;
-   }
-
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    cursor: grabbing;
+    user-select: none;
+  }
 </style>
-
